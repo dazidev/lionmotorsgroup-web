d2360d4d09fe3e950b2c063fbfc27678e45ebf16
Daniel Zipa <danielzipa@outlook.com>
2026-03-24 19:05:50 -0600

    DELETE: coments

--- src/components/private/dashboard/financials/modal/view/ViewInvestmentModal.tsx
+++ src/components/private/dashboard/financials/modal/view/ViewInvestmentModal.tsx
@@ -48,7 +48,6 @@ export const ViewInvestmentModal = ({
   if (!open) return null;
   return (
     <>
-      {/*<!-- Main modal -->*/}
       {open && (
         <div
           id="crud-modal"
@@ -56,14 +55,11 @@ export const ViewInvestmentModal = ({
           className="overflow-y-auto overflow-x-hidden fixed z-50 flex justify-center items-center w-full md:inset-0 h-screen max-h-full bg-zinc-800/90"
         >
           <div className="relative p-4 w-full max-w-full max-h-full">
-            {/*<!-- Modal content -->*/}
             <div className="relative bg-zinc-900 rounded-2xl shadow-2xl border border-stone-700">
-              {/*<!-- Modal header -->*/}
               <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t-2xl bg-zinc-800 border-stone-700">
                 <h3 className="text-2xl font-semibold">View Investments</h3>
                 <CloseButton onClick={() => setOpen(false, "manage")} />
               </div>
-              {/*<!-- Modal body -->*/}
               <ViewInvestmentTable
                 headers={["Name", "Description", "Amount", "Date", "Actions"]}
                 data={data ? data : []}

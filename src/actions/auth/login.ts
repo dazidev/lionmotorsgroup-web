1e382eb3f830a68fbdd90da4bb3c1f57fdbd0563
Daniel Zipa <danielzipa@outlook.com>
2026-08-07 20:58:01 -0600

    REMOVE: sleep

--- src/actions/auth/login.ts
+++ src/actions/auth/login.ts
@@ -1,17 +1,13 @@
 "use server";
 
 import { signIn } from "@/src/auth.config";
-import { sleep } from "@/src/utils/sleep";
 import { AuthError } from "next-auth";
-
-// ...
 
 export async function authenticate(
   prevState: string | undefined,
-  formData: FormData
+  formData: FormData,
 ) {
   try {
-    await sleep(2); //! remove after
     await signIn("credentials", formData);
   } catch (error) {
     if (error instanceof AuthError) {
